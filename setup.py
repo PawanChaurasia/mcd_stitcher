9af7e3a4280effcf8a6798796583fa811de97459
PawanChaurasia <128923968+PawanChaurasia@users.noreply.github.com>
2024-05-24 17:57:46 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mcd_stitcher',
+    version='0.1.0',
+    author = 'Pawan Chaurasia'
+    author_email = 'pchaurasia98@gmail.com'
+    description = 'MCD to Zarr conversion and stitching'
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/PawanChaurasia/mcd_stitcher',
+    project_urls={
+        'Bug Tracker': 'https://github.com/PawanChaurasia/mcd_stitcher/issues',
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    packages=find_packages(),
+    python_requires='>=3.7',
+    install_requires=[
+        'click',
+        'numpy',
+        'pandas',
+        'python_dateutil',
+        'xarray',
+        'zarr',
+        'scikit-image'
+    ],
+    entry_points={
+        'console_scripts': [
+            'imc2zarr=mcd_stitcher.converter:main',
+            'zarr_stitch=mcd_stitcher.stitcher:main',
+            'mcd_stitch=mcd_stitcher.mcd_stitch:main',
+        ],
+    },
+)
