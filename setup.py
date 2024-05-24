a4cd63208bc16a3a5f0449d87f52086905e52795
PawanChaurasia <128923968+PawanChaurasia@users.noreply.github.com>
2024-05-24 18:34:43 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mcd_stitcher',
-    version='0.1.2',
+    version='0.1.3',
     author='Pawan Chaurasia',
     author_email='pchaurasia98@gmail.com',
     description='MCD to Zarr conversion and stitching',
