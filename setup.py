fa9ac3f672c02ffe12c5640aa70fa717d7d37dbf
PawanChaurasia <128923968+PawanChaurasia@users.noreply.github.com>
2025-03-17 21:45:32 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     },
     classifiers=[
         'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: GNU General Public License v3.0 License',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Operating System :: OS Independent',
     ],
     packages=find_packages(),
