f6c1f22026d35d7d242e6b0f53da2b44cdfd74d5
PawanChaurasia <128923968+PawanChaurasia@users.noreply.github.com>
2025-06-21 12:22:38 +0100

    Update tiff_subset.py

--- mcd_stitcher/tiff_subset.py
+++ mcd_stitcher/tiff_subset.py
@@ -41,7 +41,7 @@ def write_ome_tiff(image_data: np.ndarray, channel_names: List[str], output_path
                     SizeZ="1"
                     PhysicalSizeX="1.0"
                     PhysicalSizeY="1.0"
-                    Type="uint16">
+                    Type="float32">
                 <TiffData />
                 {channels_xml}
             </Pixels>
