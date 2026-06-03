928e47d3a01c7e753b5637df16f739ff8922bfa7
Pawan Chaurasia <128923968+PawanChaurasia@users.noreply.github.com>
2026-06-03 16:16:46 +0100

    Update version to 2.3.0 and modify imports
    expose mcd_process and tiff_subset in the package API

--- mcd_stitcher/__init__.py
+++ mcd_stitcher/__init__.py
@@ -3,14 +3,16 @@ from importlib.metadata import version, PackageNotFoundError
 try:
     __version__ = version("mcd_stitcher")
 except PackageNotFoundError:
-    __version__ = "2.2.0"
+    __version__ = "2.3.0"
 
 from .mcd_stitch import mcd_stitch
-from .mcd_convert import main as mcd_convert
-from .tiff_subset import main as tiff_subset
+from .mcd_convert import mcd_convert
+from .mcd_process import mcd_process
+from .tiff_subset import tiff_subset
 
 __all__ = [
     "mcd_stitch",
     "mcd_convert",
+    "mcd_process",
     "tiff_subset",
 ]
